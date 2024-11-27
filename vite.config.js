eed7b5341c0293e257f277f2d7632d097a1e1ad5
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-27 17:21:39 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,30 @@
 import { defineConfig } from 'vite';
-import * as glob from 'glob'; // Используйте 'globby', если хотите более современный вариант.
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    sourcemap: true,
-    outDir: '../dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: glob.sync('./src/**/*.html'), // Указываем все HTML файлы
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/*.html']),
-  ],
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
