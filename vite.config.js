ae962b5011681614fc598191f8f711ae4a265611
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-27 17:16:44 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,26 @@
 import { defineConfig } from 'vite';
-import * as glob from 'glob';
+import * as glob from 'glob'; // Используйте 'globby', если хотите более современный вариант.
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig({
   root: 'src',
   build: {
     sourcemap: true,
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        entryFileNames: chunkInfo => {
-          if (chunkInfo.name === 'commonHelpers') {
-            return 'commonHelpers.js';
-          }
-          return '[name].js';
-        },
-        assetFileNames: assetInfo => {
-          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-            return '[name].[ext]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        },
-      },
-    },
     outDir: '../dist',
     emptyOutDir: true,
     assetsDir: 'assets',
+    rollupOptions: {
+      input: glob.sync('./src/**/*.html'), // Указываем все HTML файлы
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
   plugins: [
     injectHTML(),
-    FullReload(['./src/**/**.html']),
-    SortCss({
-      sort: 'mobile-first',
-    }),
+    FullReload(['./src/**/*.html']),
   ],
 });
