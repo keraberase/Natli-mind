9427bd8b387ae0d8e44d1c9cb4e421675e9761a4
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-27 19:18:09 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,41 @@ import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
+export default defineConfig({
     root: 'src',
+    base: '/Natli-mind/', // Укажите базовый путь
+    publicDir: 'src/images', // Путь, указывающий, что папка с изображениями будет доступна в `dist`
     build: {
       sourcemap: true,
-
+      outDir: '../dist',
+      emptyOutDir: true,
+      assetsDir: 'assets',
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
             }
+            return '[name].js';
           },
-          entryFileNames: 'commonHelpers.js',
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
-      outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
-});
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  });
+  
