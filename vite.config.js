9c642cb1f84d00adb52ce1df3d76b0437e9567bc
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-28 13:58:17 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -5,38 +5,27 @@ import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig({
-    root: 'src',
-    base: '/Natli-mind/', // Укажите базовый путь
-    publicDir: 'src/images', // Путь, указывающий, что папка с изображениями будет доступна в `dist`
-    build: {
-      sourcemap: true,
-      outDir: '../dist',
-      emptyOutDir: true,
-      assetsDir: 'assets',
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+  root: 'src', 
+  base: '/Natli-mind/', 
+  publicDir: 'src/images', 
+  build: {
+    sourcemap: true,
+    outDir: '../dist', 
+    emptyOutDir: true,
+    assetsDir: 'assets', 
+    rollupOptions: {
+      input: glob.sync('./src/*.html'), 
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
       },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  });
-  
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
+});
