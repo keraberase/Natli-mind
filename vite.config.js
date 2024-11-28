ccfc8203e724bf0a04101f868ab5e93025383ff5
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-28 14:57:57 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import {glob} from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  root: 'src', 
-  base: '/Natli-mind/', 
-  publicDir: 'src/images', 
-  build: {
-    sourcemap: true,
-    outDir: '../dist', 
-    emptyOutDir: true,
-    assetsDir: 'assets', 
-    rollupOptions: {
-      input: glob.sync('./src/*.html'), 
-      output: {
-        entryFileNames: '[name].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/**.html']),
-    SortCss({
-      sort: 'mobile-first',
-    }),
-  ],
+    root: 'src',
+    base: '/Natli-mind/',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
